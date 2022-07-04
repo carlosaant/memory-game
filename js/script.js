635496a01139c5a201e66e5453e95b570061e30d
Antonio Carlos <antc.minga@gmail.com>
2022-07-04 16:00:14 -0300

    Upload Project

---
+++ js/script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const C_FRONT = 'card-front';
+const C_BACK = 'card-back';
+
+let wow_elements = [
+  { name: 'shaman', path: './assets/images/shaman-icon-wow.png' },
+  { name: 'warrior', path: '...' },
+  { name: 'druid', path: '...' },
+  { name: 'darkknight', path: '...' },
+  { name: 'mage', path: '...' },
+  { name: 'hunter', path: '...' },
+  { name: 'monk', path: '...' },
+  { name: 'paladin', path: '...' },
+  { name: 'rogue', path: '...' },
+  { name: 'priest', path: '...' },
+  { name: 'warlock', path: '...' },
+  { name: 'burningcruzade', path: '...' }
+];
+
+let cards = null;
+
+// -----------------------------------------------------------------------
+
+function createCardWithElements() {
+  let cards = [];
+  for (let element of wow_elements) {
+    cards.push(createPairFromElement(element));
+  }
+
+  //flat separar os arrays dentro de cada elemento, retornando eles em um só array
+  return cards.flatMap(pair => pair);
+}
+
+function createPairFromElement(element) {
+  return [
+    {
+      id: createIdWithElement(element.name),
+      icon: element.path,
+      flipped: false
+    },
+    {
+      id: createIdWithElement(element.name),
+      icon: element.path,
+      flipped: false
+    }
+  ];
+}
+
+function createIdWithElement(element) {
+  return element + parseInt(Math.random() * 1000);
+}
+
+function embaralharCards(cards) {
+  let currentIndex = cards.length;
+  let randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [cards[randomIndex], cards[currentIndex]] = [
+      cards[currentIndex],
+      cards[randomIndex]
+    ];
+  }
+}
